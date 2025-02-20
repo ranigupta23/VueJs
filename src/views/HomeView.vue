fdaac8597b8ab870b649267a253108468ad95eb3
rani-gupta_globant <rani.gupta@globant.com>
2025-02-20 14:03:30 +0530

    S4 refs,reactive objects and non-reactive data

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,62 +1,47 @@
 <template>
  <div class="home">
+   <h2>{{ appTitle }}</h2>
+   <!-- <h3>{{ counterTitle }}</h3> -->
+   <h3>{{ counterData.title}}</h3>
     <div>
          <button @click="decreaseCounter" class="btn">-</button>
-         <span class="counter">{{ counter }}</span>
+         <!-- <span class="counter">{{ counter }}</span> -->
+         <span class="counter">{{ counterData.count }}</span>
          <button @click="increaseCounter" class="btn">+</button>
+    </div>
+    <div class="edit">
+      <h4>Edit Counter Title:</h4>
+      <!-- <input v-model="counterTitle" type="text"/> -->
+      <input v-model="counterData.title" type="text"/>
     </div>
  </div>
 </template>
 
-<!-- options API -->
-<!-- <script>
-export default{
-   data(){
-      return {
-         counter:0
-      }
-   },
-   methods:{
-      increaseCounter(){
-         this.counter++;
-      },
-      decreaseCounter(){
-         this.counter--;
-      }
-   }
-}
-</script> -->
-
-<!-- compostion api -->
-<!-- <script>
-import { ref } from 'vue'
-   export default{
-      setup(){
-         const counter=ref(0);
-         const increaseCounter=()=>{
-            counter.value++
-         }
-         const decreaseCounter=()=>{
-            counter.value--
-         }
-         return {
-            counter,
-            increaseCounter,
-            decreaseCounter,
-         }
-      }
-   }
-</script> -->
 
 <!-- new setup pattern -->
 <script setup>
-   import {ref} from 'vue'
-   const counter=ref(0);
+   import {ref,reactive} from 'vue'
+   // non reactive data
+   const appTitle="My Amazing Counter App"
+   // ref
+   const counter=ref(0),
+   counterTitle=ref('My Counter:')
+   // reactive objects
+   const counterData=reactive({
+      count:0,
+      title:"My Counter"
+   })
+   // const increaseCounter=()=>{
+   //       counter.value++
+   //    }
+   // const decreaseCounter=()=>{
+   //       counter.value--
+   //    }
    const increaseCounter=()=>{
-         counter.value++
+         counterData.count++
       }
    const decreaseCounter=()=>{
-         counter.value--
+      counterData.count--
       }
 </script>
 
@@ -69,5 +54,8 @@ import { ref } from 'vue'
    font-size: 40px;
    margin:10px;
    /* color: bisque; */
+}
+.edit{
+   margin-top: 60px;
 }
 </style>
