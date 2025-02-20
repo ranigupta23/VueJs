d09f5a360ce9976aed2edcc3180b3db2516735af
rani-gupta_globant <rani.gupta@globant.com>
2025-02-20 17:21:47 +0530

    S5 methods,computed and watch

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,47 +1,50 @@
 <template>
  <div class="home">
    <h2>{{ appTitle }}</h2>
-   <!-- <h3>{{ counterTitle }}</h3> -->
    <h3>{{ counterData.title}}</h3>
     <div>
-         <button @click="decreaseCounter" class="btn">-</button>
-         <!-- <span class="counter">{{ counter }}</span> -->
+      <button @click="decreaseCounter(2)" class="btn">--</button>
+         <button @click="decreaseCounter(1)" class="btn">-</button>
          <span class="counter">{{ counterData.count }}</span>
-         <button @click="increaseCounter" class="btn">+</button>
+         <button @click="increaseCounter(1,$event)" class="btn">+</button>
+         <button @click="increaseCounter(2,$event)" class="btn">++</button>
     </div>
+    <p>This Counter is {{ OddorEven }}</p>
     <div class="edit">
       <h4>Edit Counter Title:</h4>
-      <!-- <input v-model="counterTitle" type="text"/> -->
       <input v-model="counterData.title" type="text"/>
     </div>
  </div>
 </template>
 
 
-<!-- new setup pattern -->
+
 <script setup>
-   import {ref,reactive} from 'vue'
-   // non reactive data
+   import {reactive,computed,watch} from 'vue'
    const appTitle="My Amazing Counter App"
-   // ref
-   const counter=ref(0),
-   counterTitle=ref('My Counter:')
-   // reactive objects
    const counterData=reactive({
       count:0,
       title:"My Counter"
    })
-   // const increaseCounter=()=>{
-   //       counter.value++
-   //    }
-   // const decreaseCounter=()=>{
-   //       counter.value--
-   //    }
-   const increaseCounter=()=>{
-         counterData.count++
+   // watch
+   watch(()=>counterData.count,(newCount,oldCount)=>{
+      if(newCount===20){
+         alert('Way to go! You made it to 20!')
       }
-   const decreaseCounter=()=>{
-      counterData.count--
+   })
+   // computed properties
+   const OddorEven=computed(()=>{
+      if(counterData.count%2===0) return 'Even'
+      return 'Odd'
+   })
+   // passing parameter to methods
+   const increaseCounter=(amount,event)=>{
+         // console.log(amount)
+         // console.log(event);
+         counterData.count+=amount
+      }
+   const decreaseCounter=(amount)=>{
+      counterData.count-=amount
       }
 </script>
 
@@ -53,7 +56,6 @@
 .btn,.counter{
    font-size: 40px;
    margin:10px;
-   /* color: bisque; */
 }
 .edit{
    margin-top: 60px;
