9398f2dd89f556f89455c57add8c667f1065b8cf
rani-gupta_globant <rani.gupta@globant.com>
2025-02-24 14:31:33 +0530

    S8 vue router

---
+++ src/views/PostDetailView.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="post-detail">
+     <h1>This is post page</h1>
+     <p>Display the content of post with ID of  {{$route.params.id}}</p>
+     <div>
+         <button @click="showPostId">Show Post ID</button>
+     </div>
+     <div>
+         <button @click="goHomeIn3seconds">Go Home in 3 seconds</button>
+     </div>
+     <div>
+         <button @click="goToFirstPost">Go To First Post</button>
+     </div>
+     <p><RouterLink to="/posts">&lt; Back</RouterLink></p>
+  </div>
+ </template>
+
+ <script setup>
+ import {useRoute,useRouter} from 'vue-router'
+ const route=useRoute()
+ const router=useRouter()
+ const showPostId=()=>{
+   // console.log("Show Post Id")
+   alert(`The ID of this post is : ${route.params.id}`)
+ }
+ const goHomeIn3seconds=()=>{
+   setTimeout(()=>{
+      // router.push("/")
+      // if we want push by name
+      router.push({name:'home'})
+   },3000)
+ }
+ const goToFirstPost=()=>{
+      router.push({
+         name:'postDetail',
+         params:{
+            id:'id1'
+         }
+      })
+  
+ }
+</script>
+
+ 
